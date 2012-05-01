73a3a2137d9b507ba1524d987bfda88658428a6c
Deokjune Yi <june.yi@samsung.com>
2012-05-01 15:46:03 +0900

    [#112] synaps-api 와 synaps-storm 간 연동을 위해 rabbit-mq 사용.

--- synaps-api/synaps/rpc/__init__.py
+++ synaps-api/synaps/rpc/__init__.py
@@ -1,10 +1,44 @@
 # Copyright 2012 Samsung SDS
 # All Rights Reserved
 
+from synaps import flags
+from synaps.utils import strtime
+from synaps import log as logging
 from synaps.exception import RpcInvokeException
+import socket
+import pika, json
+
+LOG = logging.getLogger(__name__)
+FLAGS = flags.FLAGS
 
 class RemoteProcedureCall(object):
+    def __init__(self):
+        self.connect()
+    
+    def connect(self):
+        try:
+            self.conn = pika.BlockingConnection(
+                pika.ConnectionParameters(host=FLAGS.get('rabbitmq_server'))
+            )
+            
+            self.channel = self.conn.channel()
+            self.channel.queue_declare(queue='metric_queue', durable=True)
+        except socket.error:
+            raise RpcInvokeException()
+        
     def put_metric_data(self, project_id, namespace, metric_name, dimensions,
                         value, unit=None, timestamp=None):
-        # TODO: (june.yi) need to send metric to synaps-storm via zmq
-        raise RpcInvokeException()
+        
+        if not self.conn.is_open:
+            self.connect()
+        
+        timestamp = strtime(timestamp) if timestamp else None
+
+        message = {'project_id': project_id, 'namespace':namespace,
+                   'metric_name': metric_name, 'dimensions': dimensions,
+                   'value':value, 'unit':unit, 'timestamp':timestamp}
+        
+        self.channel.basic_publish(
+            exchange='', routing_key='metric_queue', body=json.dumps(message),
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
