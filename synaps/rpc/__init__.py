69a0d5da395077b67670592af15cdb595bb6d139
June Yi <gochist@gmail.com>
2013-09-15 19:38:23 +0900

    RPC connection errors are handled well

--- synaps/rpc/__init__.py
+++ synaps/rpc/__init__.py
@@ -18,13 +18,15 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from eventlet.pools import TokenPool
+from eventlet import semaphore
+from eventlet.pools import Pool
 from synaps import flags
 from synaps.utils import strtime
 from synaps import log as logging
 from synaps.exception import RpcInvokeException
 import uuid, time
 import pika, json
+from pika.exceptions import ConnectionClosed, AMQPConnectionError
 
 LOG = logging.getLogger(__name__)
 FLAGS = flags.FLAGS
@@ -38,70 +40,54 @@ SET_ALARM_STATE_MSG_ID = 0x0006
 CHECK_METRIC_ALARM_MSG_ID = 0x0010 
 
 
-def retry5_uncaught_exceptions(infunc):
-    def inner_func(*args, **kwargs):
-        last_log_time = 0
-        last_exc_message = None
-        exc_count = 0
-        for i in range(5):
-            try:
-                return infunc(*args, **kwargs)
-            except Exception as exc:
-                if exc.message == last_exc_message:
-                    exc_count += 1
-                else:
-                    exc_count = 1
-                # Do not log any more frequently than once a minute unless
-                # the exception message changes
-                cur_time = int(time.time())
-                if (cur_time - last_log_time > 60 or
-                        exc.message != last_exc_message):
-                    LOG.exception(
-                        _('Unexpected exception occurred %d time(s)... '
-                          'retrying.') % exc_count)
-                    last_log_time = cur_time
-                    last_exc_message = exc.message
-                    exc_count = 0
-                # This should be a very rare event. In case it isn't, do
-                # a sleep.
-                time.sleep(1)
-    return inner_func
+
+class RpcConnection(object):
+
+    CONN_LOCK = semaphore.Semaphore()
+
+    def __init__(self):
+        self.connect()
+
+    def connect(self):
+        host = FLAGS.get('rabbit_host')
+        port = FLAGS.get('rabbit_port')
+        LOG.info(_("Connecting to rabbit_host %s %d") % (host, port))
+
+        credentials=pika.PlainCredentials(FLAGS.get('rabbit_userid'),
+                                          FLAGS.get('rabbit_password'))
+        con_param = pika.ConnectionParameters(
+                        host=FLAGS.get('rabbit_host'),
+                        port=FLAGS.get('rabbit_port'),
+                        credentials=credentials,
+                        virtual_host=FLAGS.get('rabbit_virtual_host'))
+        queue_args = {"x-ha-policy" : "all" }
+
+        msg = 'AMQP connection exception occurred %d time(s)... retrying.'
+        max_retries = 5
+        for i in range(max_retries + 1):
+            with self.CONN_LOCK:
+                try:
+                    self.conn = pika.BlockingConnection(con_param)
+                    self.channel = self.conn.channel()
+                    self.channel.queue_declare(queue='metric_queue', 
+                                               durable=True,
+                                               arguments=queue_args)
+                    return
+
+                except AMQPConnectionError:
+                    if i < max_retries:
+                        LOG.warn(_(msg) % i)
+                        time.sleep(2 * i)
+                    else:
+                        raise
+
 
 class RemoteProcedureCall(object):
     def __init__(self):
-        self.pool = TokenPool(create=self.open_channel, min_size=10, 
-                              max_size=1000)
+        self.pool = Pool(create=RpcConnection, max_size=500)
 
-    def open_channel(self):
-        host = FLAGS.get('rabbit_host')
-        port = FLAGS.get('rabbit_port')
-        try:
-            LOG.info(_("connecting to rabbit_host %s %d") % (host, port))
-
-            credentials=pika.PlainCredentials(FLAGS.get('rabbit_userid'),
-                                              FLAGS.get('rabbit_password'))
-            con_param = pika.ConnectionParameters(
-                            host=FLAGS.get('rabbit_host'),
-                            port=FLAGS.get('rabbit_port'), 
-                            credentials=credentials,
-                            virtual_host=FLAGS.get('rabbit_virtual_host'))
-            conn = pika.BlockingConnection(con_param)
-            
-            channel = conn.channel()
-            queue_args = {"x-ha-policy" : "all" }
-            channel.queue_declare(queue='metric_queue', durable=True,
-                                  arguments=queue_args)
-            channel.confirm_delivery()
-            return channel
-
-        except Exception as e:
-            raise RpcInvokeException()
-    
-    @retry5_uncaught_exceptions
     def send_msg(self, message_id, body):
         """
-        
-        
         Args:
             message_id: int
                 ex) PUT_METRIC_DATA_MSG_ID (0x0001)
@@ -110,25 +96,34 @@ class RemoteProcedureCall(object):
             body: dict object (will be converted into json format)
             
         """
+
+        def publish(body, properties):
+            msg = 'AMQP Connection is closed %d time(s)... retrying.'
+            max_retries = 5
+            with self.pool.item() as conn:
+                for i in range(max_retries + 1):
+                    try:
+                        return conn.channel.basic_publish(exchange='',
+                                           routing_key='metric_queue',
+                                           body=body, properties=properties)
+                    except ConnectionClosed:
+                        if i < max_retries:
+                            conn.connect()
+                            LOG.warn(_(msg) % i)
+                            time.sleep(2 * i)
+                        else:
+                            raise
+
+
         if type(message_id) is not int:
             raise RpcInvokeException()
         
-
         message_uuid = str(uuid.uuid4()) 
         body.setdefault('message_id', message_id)
         body.setdefault('message_uuid', message_uuid)
         
-        expiration = '60000' if message_id == PUT_METRIC_DATA_MSG_ID else None
-        delivery_mode = 1 if message_id == PUT_METRIC_DATA_MSG_ID else 2
-        properties=pika.BasicProperties(delivery_mode=delivery_mode, 
-                                        expiration=expiration)
-        
-        with self.pool.item() as channel:
-            if not channel.is_open:
-                channel = self.open_channel()
-            
-            channel.basic_publish(exchange='', routing_key='metric_queue', 
-                                  body=json.dumps(body), properties=properties)
+        properties=pika.BasicProperties(delivery_mode=2)
+        publish(json.dumps(body), properties)
             
         LOG.info(_("send_msg - id(%03d), %s"), message_id, message_uuid)
         LOG.debug(_("send_msg - body(%s)"), str(body))
